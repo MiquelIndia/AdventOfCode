71478a8ef75de1ee7f3642bd0a24638c89e81ef1
Miquel Inida <miim@languagewire.com>
2025-01-23 20:58:53 +0100

    Added day 3 second part of the exercise

--- days/exercise3.py
+++ days/exercise3.py
@@ -1,24 +1,41 @@
 import re
+from typing import Tuple
 
 
-
-def get_list_of_operations(input_string):
+def get_list_of_operations(input_string: str) -> list[Tuple[str, str]]:
     pattern = r"mul\((-?\d+),(-?\d+)\)"
     matches = re.findall(pattern, input_string)
     return matches
 
+def calculate_valid_substring(input_string):
+    sub_strings = input_string.split("don't()")
+    valid_strings = sub_strings[0]
+    for sub_string in sub_strings[1:]:
+        sub_strings = sub_string.split("do()")
+        if len(sub_strings) > 1:
+            valid_strings += "".join(sub_strings[1:])
+    return valid_strings
 
-def extract_memory_operations_value(input_string):
-    matches = get_list_of_operations(input_string)
+def calculate_value_from_matches(matches):
     count = 0
     for match in matches:
         count += int(match[0]) * int(match[1])
     return count
 
+def extract_memory_operations_value(input_string):
+    matches = get_list_of_operations(input_string)
+    return calculate_value_from_matches(matches)
+
+def extract_conditioned_memory_operations_value(input_string):
+    valid_substring = calculate_valid_substring(input_string)
+    matches = get_list_of_operations(valid_substring)
+    return calculate_value_from_matches(matches)
+
 def main(input_path):
     with open(input_path, "r") as file:
         input_string = file.read()
         print(extract_memory_operations_value(input_string))
+        print(extract_conditioned_memory_operations_value(input_string))
 
 if __name__ == "__main__":
     main("data/input_exercise3.txt")
--- tests/test3.py
+++ tests/test3.py
@@ -3,7 +3,7 @@ import sys
 sys.path.append('./days/')
 import pytest
 
-from exercise3 import extract_memory_operations_value
+from exercise3 import extract_memory_operations_value, extract_conditioned_memory_operations_value
 
 @pytest.mark.parametrize("input_string, expected_output", [
     ("mul(2,3)", 6),
@@ -13,4 +13,15 @@ from exercise3 import extract_memory_operations_value
 ])
 def test_extract_memory_operations_value(input_string, expected_output):
     assert extract_memory_operations_value(input_string) == expected_output
+
+
+@pytest.mark.parametrize("input_string, expected_output", [
+    ("mul(2,3)", 6),
+    ("mul(3,-3)", -9),
+    ("mul(-3,-3)", 9),
+    ("xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))", 161),
+    ("xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))", 48)
+])
+def test_extract_conditioned_memory_operations_value(input_string, expected_output):
+    assert extract_conditioned_memory_operations_value(input_string) == expected_output
 

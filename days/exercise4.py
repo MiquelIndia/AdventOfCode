88db72f7e126948f2dda78e5f314a979cb4fee26
Miquel Inida <miim@languagewire.com>
2025-02-04 17:32:10 +0100

    Fixed Some Issues

--- days/exercise4.py
+++ days/exercise4.py
@@ -3,28 +3,48 @@ import copy as cp
 
 def _get_vertical_lines(lines = list()) -> list:
     vertical_lines = list()
-    for i in range(len(lines)):
+    max_length = max(len(line) for line in lines)
+    for i in range(max_length):
         vertical_line = ""
         for j in range(len(lines)):
-            vertical_line += lines[j][i]
+            if i < len(lines[j]):
+                vertical_line += lines[j][i]
+            else:
+                vertical_line += " "
         vertical_lines.append(vertical_line)
     return vertical_lines
 
 def _get_diagonal_lines(lines = list()) -> list:
     diagonal_lines = list()
-    for i in range(len(lines)):
+    max_length = max(len(line) for line in lines)
+    
+    # Get diagonals from top-left to bottom-right
+    for d in range(-len(lines) + 1, max_length):
         diagonal_line = ""
-        for j in range(len(lines)):
-            diagonal_line += lines[j][j]
-        diagonal_lines.append(diagonal_line)
+        for i in range(len(lines)):
+            j = i + d
+            if 0 <= j < len(lines[i]):
+                diagonal_line += lines[i][j]
+        if diagonal_line:
+            diagonal_lines.append(diagonal_line)
+    
+    # Get diagonals from top-right to bottom-left
+    for d in range(-len(lines) + 1, max_length):
+        diagonal_line = ""
+        for i in range(len(lines)):
+            j = len(lines[i]) - 1 - i - d
+            if 0 <= j < len(lines[i]):
+                diagonal_line += lines[i][j]
+        if diagonal_line:
+            diagonal_lines.append(diagonal_line)
+    
     return diagonal_lines
 
 
 def count_xmas_words(lines = list(), word: str = "XMAS") -> int:
-    whole_lines = cp.copy(lines)
+    whole_lines = cp.copy([line.strip() for line in lines])
     whole_lines += _get_vertical_lines(lines)
     whole_lines += _get_diagonal_lines(lines)
-
     count = 0
     for line in whole_lines:
         count += len(re.findall(word, line))
--- tests/test4.py
+++ tests/test4.py
@@ -3,7 +3,23 @@ import sys
 sys.path.append('./days/')
 import pytest
 
-from exercise4 import count_xmas_words
+from exercise4 import count_xmas_words, _get_vertical_lines, _get_diagonal_lines
+MATRIX_LINES = []
+
+@pytest.mark.parametrize("input_lines, output_lines", [
+    (["ABCD", "EFGH", "IJKL"], ["AEI", "BFJ", "CGK", "DHL"]),
+    (["ABC", "DEF", "GHI", "JKL"], ["ADGJ", "BEHK", "CFIL"]),
+
+])
+def test_get_vertical_lines(input_lines, output_lines):
+    assert _get_vertical_lines(input_lines) == output_lines
+
+@pytest.mark.parametrize("input_lines, output_lines", [
+    (["ABCD", "EFGH", "IJKL"], ["I", "EJ", "AFK", "BGL", "CH", "D", "L", "HK", "DGJ", "CFI", "BE", "A"]),
+
+])
+def test_get_diagonal_lines(input_lines, output_lines):
+    assert _get_diagonal_lines(input_lines) == output_lines
 
 @pytest.mark.parametrize("input_lines, expected", [
     (["MMMSXXMASM", "MSAMXMSMSA", "AMXSXMAAMM", "MSAMASMSMX", "XMASAMXAMM", "XXAMMXXAMA", "SMSMSASXSS", "SAXAMASAAA", "MAMMMXMMMM", "MXMXAXMASX"], 18)
